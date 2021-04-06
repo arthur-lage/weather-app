37de5649a3a6cc3b030411a69a12f6b7755a495a
arthur-lage <arthurlage2006@gmail.com>
2021-04-05 23:46:51 -0300

    HTML, CSS and JS ready

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 const apikey = "99f9d1e992ebf7091f2f24e748535ae1";
+const cityInput = document.getElementById("city-input")
+const form = document.getElementById("city-form")
+const temperatureText = document.getElementById("temperature-text")
+const cityText = document.getElementById("city-text")
+const maxTemperatureText = document.getElementById("max-temperature-text")
+const minTemperatureText = document.getElementById("min-temperature-text")
+const  cloudQuantity = document.getElementById("cloud-quantity-text")
+const results = document.getElementById("results")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    city = cityInput.value
+    getData(city)
+})
+                     //http://api.openweathermap.org/data/2.5/weather?q=London&appid=99f9d1e992ebf7091f2f24e748535ae1
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+
+async function getData(city){
+    const resp = await fetch(url(city), { origin: "cors" })
+    const respData = await resp.json()
+
+    console.log(respData)
+
+    showWeather(respData)
+}
+
+function showWeather(data){
+    results.style.opacity = "1"
+    results.style.pointerEvents = "auto"
+    cityText.innerHTML = data.name
+    maxTemperatureText.innerHTML = `Temperatura máxima: ${kelvinToCelsius(data.main.temp_max).toFixed(2)} ºC`
+    temperatureText.innerHTML = `${kelvinToCelsius(data.main.temp).toFixed(2)} ºC`
+    minTemperatureText.innerHTML = `Temperatura mínima: ${kelvinToCelsius(data.main.temp_min).toFixed(2)} ºC`
+    cloudQuantity.innerHTML = `Quantidade de nuvens: ${data.clouds.all}`
+}
+
+function kelvinToCelsius(temperature){
+    const convertedTemp = temperature - 273.15
+    return convertedTemp
+}
