87a3a3abeac50e30d1880e6feefd0f752f23f90b
arthur-lage <arthurlage2006@gmail.com>
2021-04-06 11:02:58 -0300

    new consult button

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 const apikey = "99f9d1e992ebf7091f2f24e748535ae1";
-const cityInput = document.getElementById("city-input")
-const form = document.getElementById("city-form")
-const temperatureText = document.getElementById("temperature-text")
-const cityText = document.getElementById("city-text")
-const maxTemperatureText = document.getElementById("max-temperature-text")
-const minTemperatureText = document.getElementById("min-temperature-text")
-const  cloudQuantity = document.getElementById("cloud-quantity-text")
-const results = document.getElementById("results")
+const cityInput = document.getElementById("city-input");
+const form = document.getElementById("city-form");
+const temperatureText = document.getElementById("temperature-text");
+const cityText = document.getElementById("city-text");
+const maxTemperatureText = document.getElementById("max-temperature-text");
+const minTemperatureText = document.getElementById("min-temperature-text");
+const cloudQuantity = document.getElementById("cloud-quantity-text");
+const results = document.getElementById("results");
+const button = document.getElementById("consult-button")
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    city = cityInput.value
-    getData(city)
+    e.preventDefault();
+    city = cityInput.value;
+    getData(city);
 })
-                     //http://api.openweathermap.org/data/2.5/weather?q=London&appid=99f9d1e992ebf7091f2f24e748535ae1
-const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+
+button.addEventListener("submit", (e) => {
+    e.preventDefault();
+    city = cityInput.value;
+    getData(city);
+})
+
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 async function getData(city){
-    const resp = await fetch(url(city), { origin: "cors" })
-    const respData = await resp.json()
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
 
-    console.log(respData)
-
-    showWeather(respData)
+    showWeather(respData);
 }
 
 function showWeather(data){
-    results.style.opacity = "1"
-    results.style.pointerEvents = "auto"
-    cityText.innerHTML = data.name
-    maxTemperatureText.innerHTML = `Temperatura máxima: ${kelvinToCelsius(data.main.temp_max).toFixed(2)} ºC`
-    temperatureText.innerHTML = `${kelvinToCelsius(data.main.temp).toFixed(2)} ºC`
-    minTemperatureText.innerHTML = `Temperatura mínima: ${kelvinToCelsius(data.main.temp_min).toFixed(2)} ºC`
-    cloudQuantity.innerHTML = `Quantidade de nuvens: ${data.clouds.all}`
+    results.style.opacity = "1";
+    results.style.pointerEvents = "auto";
+    cityText.innerHTML = data.name;
+    maxTemperatureText.innerHTML = `Temperatura máxima: ${kelvinToCelsius(data.main.temp_max).toFixed(2)} ºC`;
+    temperatureText.innerHTML = `Temperatura atual: ${kelvinToCelsius(data.main.temp).toFixed(2)} ºC`;
+    minTemperatureText.innerHTML = `Temperatura mínima: ${kelvinToCelsius(data.main.temp_min).toFixed(2)} ºC`;
+    cloudQuantity.innerHTML = `Quantidade de nuvens: ${data.clouds.all}`;
 }
 
 function kelvinToCelsius(temperature){
-    const convertedTemp = temperature - 273.15
-    return convertedTemp
+    const convertedTemp = temperature - 273.15;
+    return convertedTemp;
 }
